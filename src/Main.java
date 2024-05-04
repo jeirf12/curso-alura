4cb614921d66fc2219b2dd9cad2fa8c5ac35cf23
Jhonfer Ruiz <jhonfer.ruiz@gmail.com>
2024-05-04 17:01:13 -0500

    added validation without evaluations and correction grammatical words

--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,17 @@ public class Main {
     Double suma = 0.0; 
     Integer contador = 0;
     List<Double> calificaciones = new ArrayList<>();
-    String nombre = Consola.leer("Ingrese el nombre de la pelicula que desea calificar: ", "");
-    Consola.escribirSaltandoLinea("Ingrese las calificaciones de la pelicula " + nombre);
+    String nombre = Consola.leer("Ingrese el nombre de la película que desea calificar: ", "");
+    Consola.escribirSaltandoLinea("Ingrese las calificaciones de la película " + nombre);
 
     while (!bandera) {
-      Double calificacion = Consola.leer("Ingrese calificacion (1 a 5) siendo 5 la mejor: ", -1.0);
+      Double calificación = Consola.leer("Ingrese calificación (1 a 5) siendo 5 la mejor: ", -1.0);
 
-      if(calificacion < -1 || calificacion > 5) {
-        Consola.escribirSaltandoLinea("La calificacion no es correcta. Por favor, intente nuevamente");
+      if(calificación < -1 || calificación > 5) {
+        Consola.escribirSaltandoLinea("La calificación no es correcta. Por favor, intente nuevamente");
       } else {
-        calificaciones.add(calificacion);
-        suma += calificacion;
+        calificaciones.add(calificación);
+        suma += calificación;
         contador++;
       }
 
@@ -32,9 +32,11 @@ public class Main {
 
     if(contador > 0) {
       Double promedio = suma / contador;
-      Consola.escribirSaltandoLinea("La calificacion media de la pelicula " + nombre + " es: " + promedio);
+      Consola.escribirSaltandoLinea("La calificación media de la película " + nombre + " es: " + promedio);
       Consola.escribirSaltandoLinea("El detalle de las calificaciones es el siguiente: ");
       Consola.escribirSaltandoLinea(calificaciones);
+    } else {
+      Consola.escribirSaltandoLinea("No se ingresaron calificaciones validas para hacer el calculo");
     }
   }
 }
